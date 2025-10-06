21fc3cd41f6250493708105f794f8e3908201d6e
linxiw <linxiw@unc.edu>
2025-10-06 18:54:26 -0400

    Initial commit: VCIC voting system

---
+++ precache-manifest.d4a1929f119f052da49ddf35f05d28f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c71851e2daf2a57cad5ffb4840f618",
+    "url": "./index.html"
+  },
+  {
+    "revision": "077d2aa234ff48ec2b0d",
+    "url": "./static/css/main.cf5e7ec1.chunk.css"
+  },
+  {
+    "revision": "c7acb31eae37fba7bfe7",
+    "url": "./static/js/2.31627de0.chunk.js"
+  },
+  {
+    "revision": "530446141ed040f204240e59baa92715",
+    "url": "./static/js/2.31627de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077d2aa234ff48ec2b0d",
+    "url": "./static/js/main.04202b59.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
